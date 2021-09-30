2c91427dd5444e2be807787f213430ffc3cf18ca
unknown <ishtiaqahmad0331@gmail.com>
2021-09-30 13:29:10 +0200

    added basic linear methods we are gonna worke

--- R/lab04.R
+++ R/lab04.R
@@ -1,6 +1,6 @@
 # Hello, world!
 #
-# This is an example function named 'hello' 
+# This is an example function named 'hello'
 # which prints 'Hello, world!'.
 #
 # You can learn more about package authoring with RStudio at:
@@ -13,6 +13,11 @@
 #   Check Package:             'Ctrl + Shift + E'
 #   Test Package:              'Ctrl + Shift + T'
 
-hello <- function() {
-  print("Hello, world!")
-}
+#' Multiple linear regression package in R
+#' @return
+#' @export linreg
+#'
+#'
+#' @examples
+
+linreg <- setRefClass("linreg", fields = (),methods= list(),print=(),residv =(),prediction=(), coefficients=(), plot=(), summary=())
