5706f11d56ec38dd26a368c3455379ff56f2460c
Uzair khan <muhammad.uzair@cachecloud.com>
2021-10-02 18:52:51 +0200

    build and added function to developed

--- R/lab04.R
+++ R/lab04.R
@@ -1,23 +1,22 @@
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
-# You can learn more about package authoring with RStudio at:
-#
-#   http://r-pkgs.had.co.nz/
-#
-# Some useful keyboard shortcuts for package authoring:
-#
-#   Install Package:           'Ctrl + Shift + B'
-#   Check Package:             'Ctrl + Shift + E'
-#   Test Package:              'Ctrl + Shift + T'
-
 #' Multiple linear regression package in R
 #' @return
-#' @export linreg
+#' @export
 #'
 #'
 #' @examples
+#' linreg()
+linreg <- setRefClass("linreg",
+                      fields = list(
+                        sqrtstresiduals = "matrix",
+                        export_formula = "formula",
+                        export_data = "character"),
+                      methods= list(
+                        print=function(){},
+                        plot =function(){},
+                        resid=function(){},
+                        pred=function(){},
+                        coef=function(){},
+                        summary=function(){}
+                        )
+                      )
 
-linreg <- setRefClass("linreg", fields = (),methods= list(),print=(),residv =(),prediction=(), coefficients=(), plot=(), summary=())
